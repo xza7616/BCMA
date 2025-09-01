c391ac540d0b40041ee7777d618bd85b8a3f4a13
xza7616 <84484801+xza7616@users.noreply.github.com>
2025-09-01 09:00:42 +0800

    Update Sparse_Cross_Attention.py

--- toolbox/models/text1_Net/models/Sparse_Cross_Attention.py
+++ toolbox/models/text1_Net/models/Sparse_Cross_Attention.py
@@ -11,9 +11,9 @@ class Mlp(nn.Module):
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
         self.fc1 = nn.Linear(in_features, hidden_features)
+        self.fc2 = nn.Linear(hidden_features, out_features)
         self.dwconv = DWConv(hidden_features)
         self.act = act_layer()
-        self.fc2 = nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(drop)
         self.apply(self._init_weights)
 
@@ -47,36 +47,21 @@ class CMlp(nn.Module):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
-        self.fc1 = nn.Conv2d(in_features, hidden_features, 1)
+        self.conv1 = nn.Conv2d(in_features, hidden_features, 1)
+        self.conv2 = nn.Conv2d(hidden_features, out_features, 1)
         self.act = act_layer()
-        self.fc2 = nn.Conv2d(hidden_features, out_features, 1)
         self.drop = nn.Dropout(drop)
 
     def forward(self, x):
         # print(x.shape)
-        x = self.fc1(x)
+        x = self.conv1(x)
         x = self.act(x)
         x = self.drop(x)
-        x = self.fc2(x)
+        x = self.conv2(x)
         x = self.drop(x)
         return x
 
-
-class DWConv(nn.Module):
-    def __init__(self, dim=768):
-        super(DWConv, self).__init__()
-        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
-
-    def forward(self, x, H, W):
-        B, N, C = x.shape
-        x = x.transpose(1, 2).view(B, C, H, W)
-        x = self.dwconv(x)
-        x = x.flatten(2).transpose(1, 2)
-
-        return x
-
-
-class Attention(nn.Module):
+class CrossAttention(nn.Module):
     def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
         super().__init__()
         self.num_heads = num_heads
@@ -159,11 +144,22 @@ def alter_unsparse(x, Ho, Hp, Wo, Wp, C, sparse_size=8):
     out = out.permute(0, 3, 1, 2)
 
     return out
+    
+class DWConv(nn.Module):
+    def __init__(self, dim=768):
+        super(DWConv, self).__init__()
+        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
 
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        x = x.transpose(1, 2).view(B, C, H, W)
+        x = self.dwconv(x)
+        x = x.flatten(2).transpose(1, 2)
+
+        return x
 
 class CBlock(nn.Module):
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+    def __init__(self, dim, mlp_ratio=4., drop=0.,drop_path=0., act_layer=nn.GELU):
         super().__init__()
         self.pos_embed = nn.Conv2d(dim, dim, 3, padding=1, groups=dim)
         self.norm1 = nn.BatchNorm2d(dim)
@@ -191,7 +187,7 @@ class Sparse_Cross_Attention(nn.Module):
         self.norm1_q = norm_layer(dim)
         self.norm1_kv = norm_layer(dim)
 
-        self.attn = Attention(
+        self.attn = CrossAttention(
             dim,
             num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
             attn_drop=attn_drop, proj_drop=drop)
@@ -235,38 +231,3 @@ class Sparse_Cross_Attention(nn.Module):
 
 layer_scale = True
 init_value = 1e-6
-
-if __name__ == '__main__':
-
-    B, C, H, W = 1, 64, 120, 160
-    input_tensor1 = torch.randn(B, C, H, W)
-    input_tensor2 = torch.randn(B, C, H, W)
-
-    dim = C
-    num_heads = 4
-    sparse_size = 4
-    mlp_ratio = 4.0
-    qkv_bias = True
-    drop = 0.1
-    attn_drop = 0.1
-    drop_path = 0.1
-
-    sablock = Sparse_Cross_Attention(
-        dim=dim,
-        num_heads=num_heads,
-        sparse_size=sparse_size,
-        mlp_ratio=mlp_ratio,
-        qkv_bias=qkv_bias,
-        drop=drop,
-        attn_drop=attn_drop,
-        drop_path=drop_path
-    )
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    sablock = sablock.to(device)
-
-    input_tensor1 = input_tensor1.to(device)
-    input_tensor2 = input_tensor2.to(device)
-    output = sablock(input_tensor1, input_tensor2)
-
-    print(f"Output shape: {output.shape}")
